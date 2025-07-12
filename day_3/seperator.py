9a0527cbd6b289430517ea5200aed0b8c6152231
Arun Kumar <java.kumar.arun@gmail.com>
2025-07-12 10:29:24 +0530

    separator

---
+++ day_3/seperator.py
@@ -0,0 +1,21 @@
+print("Apple", "Banana", "Orange", sep=" - ")
+
+print("Apple", "Banana", "Orange", sep=" ")
+print("Apple", "Banana", "Orange")
+
+
+name="Anjali"
+
+name="Anjali"
+print("My name is",name,"demo", sep=".")
+# My name is.Anjali.demo
+print("My name is",name,"", sep=".")
+# My name is.Anjali.
+print("My name is",name,"")
+# My name is Anjali 
+print("My name is",name,".")
+# My name is Anjali .
+print("My name is",name,".", sep="")
+# My name isAnjali.
+print("My name is ",name,".", sep="")
+# My name is Anjali.
