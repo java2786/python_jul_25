7035310b93cdc92c6e9b215bde9c9e34b27fc595
Arun Kumar <java.kumar.arun@gmail.com>
2025-07-13 10:31:09 +0530

    13Jul2025

---
+++ day_4/student_info_system.py
@@ -0,0 +1,13 @@
+# Infromatin collector
+name = input("Enter student's name: ")
+address = input("Enter student's address: ")
+college = input("Enter student's college: ")
+age = input("Enter student's age: ")
+# print(type(age))
+# Student Information Display
+print("======Student Information System======")
+print("Student's name:", name)
+print("Student's age:", age)
+print("Student's college:", college)
+print("Student's address:", address)
+print("======Happy learning======")
