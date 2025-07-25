78396ff3bf1d756f14761a2e72694a459f236dd0
Arun Kumar <java.kumar.arun@gmail.com>
2025-07-25 10:45:57 +0530

    24jul

---
+++ day_8_String/demo.py
@@ -0,0 +1,10 @@
+name = "I like python and python likes me."
+
+print("First char",name[0])
+print("Second char",name[1])
+
+print("Last char",name[len(name)-1])
+print("Last char", name[-1])
+
+print("Occurrance of p",name.index("p"))
+print("Occurrance of p after eighth char",name.index("p",8))
---
+++ day_8_String/string_methods.py
@@ -0,0 +1,37 @@
+fname = 'ramesh'
+lname = 'MITTAL'
+
+# Hello - Ramesh Mittal
+print("Hello -",fname,lname) # Hello - ramesh MITTAL
+print("Hello -",fname.upper(),lname.lower()) # Hello - RAMESH mittal
+
+print("Capitalize: ",fname[0].upper(),fname[1:].lower(), sep="")
+print("Capitalize:",lname.capitalize())
+
+# output
+# print("Hello -",fname.capitalize(),lname.capitalize())
+output = "Hello - "+fname.capitalize()+" "+lname.capitalize()
+print(output)
+
+# replace Hello with Welcome
+output = output.replace("Hello", "Welcome")
+print(output)
+
+
+
+# first char
+# print("First char",fname[0])
+# print("total chars",len(fname))
+# print("Last char",fname[len(fname)-1])
+# print("Last char",fname[-1])
+# print("substring",fname[0: 3])
+
+
+
+username = input("Enter your name: ")
+print("Welcome", username.strip())
+
+
+# Home work - Input email and verify if it is valid email address
+# simple
+arunkumar@gmail.com 
