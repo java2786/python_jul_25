1fe5d1213b2c5822a482a990ee5f2a54faa75c71
Arun Kumar <java.kumar.arun@gmail.com>
2025-08-11 16:05:31 +0530

    mini project

---
+++ day_14_mini_project/lms.py
@@ -0,0 +1,71 @@
+import os 
+
+BOOKS_FILE = "books.txt"
+USERS_FILE = "users.txt"
+def load_books():
+    # print("File is available",os.path.exists(BOOKS_FILE))
+    if(os.path.exists(BOOKS_FILE)):
+        with open(BOOKS_FILE, 'r') as file: 
+            # print("reading lines")
+            lines = []
+            for line in file:
+                # print(line.strip())
+                lines.append(line.strip())
+                
+            return lines  
+    else:
+        return []
+    
+def user_registeration():
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    with open(USERS_FILE, 'a') as file: 
+        file.write(f"{name},{email},{password},0\n")
+    
+def user_login():
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    found_user = None
+    with open(USERS_FILE, 'r') as file:
+        for line in file:
+            user_list = line.strip().split(",")
+            if email == user_list[1] and password == user_list[2]:
+                print("valid user")
+                found_user = (user_list[0], user_list[1], user_list[3])
+                return found_user
+            else:
+                print("invalid user")
+        return None
+
+def main_menu():
+    while True:
+        print("***** Welcome to Library *****")     
+        print("1. Open Account")
+        print("2. Login")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if(choice == "1"):
+            user_registeration() 
+        elif choice == "2":
+            user = user_login() 
+            print(user)
+            if user == None:
+                print("Login failed")
+            else:
+                print("Login successful")
+        elif choice == "3":
+            break 
+        else:
+            print("Invalid choice. Try again.")
+
+# books = load_books()
+# print(books)
+
+# user_registeration()
+
+# user = user_login()
+# print("login: ", user)
+
+main_menu()
---
+++ doubts/doubt.py
@@ -0,0 +1,24 @@
+# list
+marks = [83, 81, 89, 91] # update is possible, read via index
+marks[1] = 87
+
+# tutple
+myTuple = ("Mobile", 12000, "8GB")
+myTuple[1]
+# myTuple[1] = 13000
+
+# dictionary
+contactBook = {
+    "ramesh": ("ram@gmail.com", 98989898), 
+    "mahesh": ("mahi@gmail.com", 87687689)
+}
+contactBook['ramesh']
+
+# set - remove duplicates
+musicPlayer = {"ae vatab ae vatan", "Dil hai hindustani"}
+musicPlayer.add("Dil hai hindustani")
+musicPlayer.add("Bharat mata ki jai")
+
+print("Music",musicPlayer)
+
+
